e5d9a5460809395d70f28698b5eca8e786dfe8fc
bimalraj1997 <42884690+bimalraj1997@users.noreply.github.com>
2023-07-29 16:21:06 +0530

    Delete MICROSERVICE directory

--- MICROSERVICE/OrderService/src/main/java/com/microserviceprjct/OrderService/service/OrderService.java
+++
@@ -1,7 +0,0 @@
-package com.microserviceprjct.OrderService.service;
-
-import com.microserviceprjct.OrderService.model.OrderRequest;
-
-public interface OrderService {
-    long placeOrder(OrderRequest orderRequest);
-}
